73e2a047ca444902012162098e838b8e489f54c0
randallkanna <randallkanna@gmail.comm>
2018-07-10 08:57:35 -0700

    adds app name and starts app js functionality

--- src/App.js
+++ src/App.js
@@ -87,25 +87,30 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
-        </nav>
+      <div>
+        <h1>Test</h1>
 
-        <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
-              <button onClick={this.handleClick.bind(this)}>Set storage</button>
-            </div>
-          </div>
-        </main>
+        
       </div>
+      // <div className="App">
+      //   <nav className="navbar pure-menu pure-menu-horizontal">
+      //       <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
+      //   </nav>
+      //
+      //   <main className="container">
+      //     <div className="pure-g">
+      //       <div className="pure-u-1-1">
+      //         <h1>Good to Go!</h1>
+      //         <p>Your Truffle Box is installed and ready.</p>
+      //         <h2>Smart Contract Example</h2>
+      //         <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
+      //         <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
+      //         <p>The stored value is: {this.state.storageValue}</p>
+      //         <button onClick={this.handleClick.bind(this)}>Set storage</button>
+      //       </div>
+      //     </div>
+      //   </main>
+      // </div>
     );
   }
 }
