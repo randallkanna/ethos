45b2001b243af06a6be0efdca9107393e5013875
randallkanna <randallkanna@gmail.comm>
2018-07-18 20:06:02 -0700

    adds test doc

--- test/Fund.js
+++ test/Fund.js
@@ -13,8 +13,8 @@ contract('Fund', function(accounts) {
   });
 
   /**
-    getFundHashByAddress.
-    Verifies that a users ipfsHash is properly stores with their address
+    getFundHashByAddress
+    Verifies that a users ipfsHash is properly stored with their address. Tests that the struct is created properly.
   **/
   it("a user can store a ipfsHash of their fund information when creating a fund", async () => {
     const ipfsHash = "testABC123";
@@ -26,6 +26,10 @@ contract('Fund', function(accounts) {
     assert.strictEqual(storedFundHash, ipfsHash, 'fund was stored properly');
   });
 
+  /**
+    donateToFund
+    Verifies that the contract withdraws from a user when they donate to a fund.
+  **/
   it("should withdraw the amount deposited by a user when they donate to a fund", async () => {
     const fundDeposit = web3.toBigNumber(2);
     const ipfsHash = "testABC123";
@@ -44,6 +48,10 @@ contract('Fund', function(accounts) {
     // assert.strictEqual(senderOriginalBalance - fundDeposit - gasUsed, senderNewBalance, 'should withdraw from donaters account');
   });
 
+  /**
+    donateToFund
+    Verifies that the contract deposits the right amount into the selected fund
+  **/
   // // TODO: Implement this test
   it("should deposit correct amount into the selected fund", async () => {
     const fundDeposit = web3.toBigNumber(2);
@@ -58,6 +66,10 @@ contract('Fund', function(accounts) {
     // assert.equal(deposit.plus(1000).toString(), balance, 'deposit amount incorrect, check deposit method');
   });
 
+  /**
+    stop()
+    Verifies that the contract does not allow users to call certain functions when the contract is stopped
+  **/
   it("should not allow a user to call certain functions while stopped", async () => {
     await fund.stop();
     await fund.set("ipfsHash");
@@ -65,6 +77,10 @@ contract('Fund', function(accounts) {
     assert.equal(getipfshHash, "ipfsHash");
   });
 
+  /**
+    start()
+    Verifies that the contract returns to normal state after the contract is restarted
+  **/
   it('should return to normal contract state after stop contract is over', async () => {
     await fund.stop();
     await fund.start();
@@ -74,6 +90,9 @@ contract('Fund', function(accounts) {
     assert.equal(getipfshHash, "ipfsHash");
   });
 
+  /**
+  TODO: Insert comments
+  **/
   // it("should withdraw the entire balance from the fund to the fund owners account", async () => {
   //   const fundDeposit = 2;
   //   const title = "Wildlife Fund ABC";
@@ -93,6 +112,9 @@ contract('Fund', function(accounts) {
   //   assert.strictEqual(fundOwnerOriginalBalance + fundDeposit - gasUsed, fundOwnerNewBalance, 'transfers proper amount');
   // });
 
+  /**
+    TODO: insert comments
+  **/
   // it("should not allow owners to contribute to their own fund", async() => {
   //   // TODO: Add test
   // })
